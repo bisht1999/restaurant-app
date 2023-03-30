759c368a5c7e9763551630023a6d7c349eea36ea
Aman <bishtaman08@gmail.com>
2023-03-30 22:25:03 +0530

    next commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,14 +5,14 @@ const Cart = (props) =>{
 
     const cartItems=[{id:'c1',name:'shusi',amount:2,price:12.99}
 ].map((item)=><li>{item.name}</li>)
-return  <Modal>
+return  <Modal onClose={props.onClose}>
     {cartItems}
     <div className={classes.total}>
         <span>Toatal Amount</span>
         <span>35.62</span>
     </div>
     <div className={classes.button}>
-        <button className={classes['button--alt']}>close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>close</button>
         <button className={classes.action}>Order</button>
     </div>
 
